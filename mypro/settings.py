66edbd0184977f1e915c28768243ab87e7b130a8
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-07-31 00:56:34 +0530

    defaltu database use

--- mypro/settings.py
+++ mypro/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'home',
-        'USER': 'root',
-        'PASSWORD': 'Raghav@@123',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
